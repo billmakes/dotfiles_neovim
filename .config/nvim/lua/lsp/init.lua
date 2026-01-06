e051b40d347932402e08b77d3c9c0c9f35946352
billmakes <billmakes@protonmail.com>
2026-01-06 14:08:57 -0500

    simplify lsp

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,20 +1,5 @@
 require("lsp.lua_ls")
-require("lsp.vtsls")
--- require("lsp.ts_ls")
-require("lsp.vscode")
-require("lsp.pyright")
 
-vim.lsp.enable({
-	"bashls",
-	"lua_ls",
-	"vtsls",
-	"cssls",
-	"jsonls",
-	"html",
-	-- "ts_ls",
-	"yamlls",
-	"pyright",
-})
 vim.diagnostic.config({
 	signs = true,
 	underline = true,
